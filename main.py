b47bab08c13f9a8e06be6c340ce7e2704c761b20
PaulinoMoskwa <pau.moskwa@gmail.com>
2022-05-21 14:28:18 +0200

    Update

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ from sklearn.linear_model             import LogisticRegression
 ### Title
 ###-----------------------------------------------------------------------------------------
 if st.button('Hello 👋'):
-    js = "window.open('https://paulinomoskwa.github.io/Hello/')"  # New tab or window
-    #js = "window.location.href = 'https://paulinomoskwa.github.io/Hello/'"  # Current tab
+    #js = "window.open('https://paulinomoskwa.github.io/Hello/')"  # New tab or window
+    js = "window.location.href = 'https://paulinomoskwa.github.io/Hello/'"  # Current tab
     html = '<img src onerror="{}">'.format(js)
     div = Div(text=html)
     st.bokeh_chart(div)
